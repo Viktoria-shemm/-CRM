8b2687b751b7d33d5050a3cd23f557544d12eedc
Виктория <vshemshukevich@outlook.com>
2025-01-28 13:48:39 +0300

    init

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 {
-  const product_name = "iphone";
-  const product_quantity = 1;
-  const product_category = "mobile devices";
-  const product_price = 90000;
+  const productName = "iphone";
+  const productQuantity = 1;
+  const productCategory = "mobile devices";
+  const productPrice = 90000;
 
-  console.log(product_name);
+  console.log(productName);
 }
 
 {
